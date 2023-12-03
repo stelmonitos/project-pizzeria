e52a18bfd202152c4c8131d2513880dd6435c3ba
Jakub Stelmasiak <stelmon3@gmail.com>
2023-12-03 15:00:12 +0100

    Add total price // breakfast need fixing

--- src/js/script.js
+++ src/js/script.js
@@ -134,14 +134,27 @@ const select = {
       for(let paramId in thisProduct.data.params) {
         //determine param value, e.g paramId = 'toppings', param = {label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        // console.log(paramId, param);
         
         // for every option in this category
         for(let optionId in param.options) {
           // dtermine option Value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          // ZACZNIJ OD KOMENTARZY SPROBUJ 30 MINUT POTEM PIERWSZA PODPOWIEDZ!!!!
-          console.log(optionId, option);
+          // console.log(optionId, option);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if(option.default !== true) {
+              //add option price to price variable
+              price += option.price;
+            }
+          } else {
+            // check if the option is default
+            if(option.default == true)
+              // reduce price variable
+            price -= option.price;
+          }
         }
       }
 
@@ -182,4 +195,3 @@ const select = {
 
   app.init();
 }
-
