757e65f1d3a35895cb0339aca74cf569c0728ade
Jakub Stelmasiak <stelmon3@gmail.com>
2024-01-17 17:54:33 +0100

    Add BaseWidget

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -22,7 +22,9 @@ class Booking {
     initWidgets() {
         const thisBooking = this;
         thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.dom.peopleAmount.addEventListener('updated', function () {});
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.dom.hoursAmount.addEventListener('updated', function () {});
     }
 }
 
